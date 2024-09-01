4b7cb602f673824aef60c9ab3f7ce0ac94cac052
Golam Muntakim <seams123456789@gmail.com>
2024-09-01 09:47:55 -0700

    prototypal inheritance

--- backend/class.js
+++ backend/class.js
@@ -1,4 +1,4 @@
-class CreateUser{
+class User{
     constructor(firstName, lastName, age){
         this.firstName = firstName,
         this.lastName  = lastName,
@@ -11,4 +11,30 @@ class CreateUser{
         return this.firstName + ' ' + this.lastName
     }
 }
-const user1 = new CreateUser ( 'siam', 'Ahmed' , 24)
+class Student extends User{
+    constructor(firstName, lastName,age ,standard){
+        super(firstName, lastName,age)
+        this.standard = standard
+    }
+    study(){
+        console.log('studying');
+    }
+}
+class Employe{
+    // constructor(firstName, lastName, age, company){
+    //     this.firstName = firstName,
+    //     this.lastName  = lastName,
+    //     this.age = age
+    //     this.company = company
+    // }
+    // getBirthYear(){
+    //     return new Date().getFullYear() - this.age
+    // }
+    // getFullName(){
+    //     return this.firstName + ' ' + this.lastName
+    // }
+}
+// const user1 = new User ( 'siam', 'Ahmed' , 24)
+const student1 = new Student ( 'siam', 'Ahmed' , 24, 'LLB')
+// const student2 = new User ( 'Arman', 'Ahmed' , 20, '10')
+const employ1 = new Employe ( 'Arman', 'Ahmed' , 20, 'Google')

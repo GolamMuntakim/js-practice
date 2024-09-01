1db682695ac42966fc32bf657b33b6bf4a037511
Golam Muntakim <seams123456789@gmail.com>
2024-09-01 00:21:02 -0700

    classes

--- backend/oop.js
+++ backend/oop.js
@@ -10,13 +10,28 @@
 // function getAgeYear(){
 //     return new Date().getFullAge() - this.age
 // }
-function createUser(firstName, lastName, age){
-    const user = {
-        firstName,
-        lastName,
-        age,
-        getAgeYear
-    }
-    return user
+// function createUser(firstName, lastName, age){
+//     const user = {
+//         firstName,
+//         lastName,
+//         age,
+//         getAgeYear
+//     }
+//     return user
+// }
+// const user1 = createUser("siam", "ahmed", 24)
+
+
+function CreateUser(firstName, lastName, age){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
 }
-const user1 = createUser("siam", "ahmed", 24)
+CreateUser.prototype.getBirthYear = function(){
+    return new Date().getFullYear() - this.age
+}
+CreateUser.prototype.getFullName = function(){
+    return this.firstName + ' ' + this.lastName
+}
+
+const user1 = new CreateUser('siam', 'ahmed', 24)
